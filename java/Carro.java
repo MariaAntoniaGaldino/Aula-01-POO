06a7748b608b9b198e436d02974c99467013032a
Maria Antonia Galdino <107472413+MariaAntoniaGaldino@users.noreply.github.com>
2023-03-08 20:56:03 -0400

    Add files via upload

---
+++ java/Carro.java
@@ -0,0 +1,29 @@
+public class Carro {
+    String fabricante;
+    String modelo;
+    String cor; 
+    Int anoDeFabricacao;
+
+    void Ligar(){
+        if(ligado){
+            return;
+        }else{
+            System.out.println(x:"Carro ligado");
+            ligado = true;
+        }
+    }
+
+    void Desligado(){
+        if(!ligado){
+            return;
+        }else{
+            System.out.println(x:"Carro desligado");
+            ligado = false;
+        }
+    }
+
+    
+    
+
+    Entidade propriedade = new Entidade();
+}
---
+++ java/ProgramaCarro.java
@@ -0,0 +1,32 @@
+public class ProgramaCarro {
+    public static void main (string[] args) {
+        System.out.println("ola");
+  
+
+  Carro uno = new Carro ();
+  uno.anoDeFabricacao = 2022;
+  uno.cor = "vermelho";
+  uno.fabricante = "Fiat";
+  uno.modelo = "Way";
+
+  System.out.println(uno.anoDeFabricacao);
+  uno.proprietario.nome = "Leticia";
+  System.out.println(uno.proprietario.nome);
+
+  System.out.println(uno.ResumoCarro());
+
+  uno.ligar();
+  uno.ligar();
+  }
+
+  System.out.println(uno.ResumoCarro());
+  System.out.println(uno.ResumoCarro("KmRodado: "+uno.KmRodado);
+  uno.Rodar(kmRodado:50);
+  System.out.println("kmrodado:"+uno.kmRodado);
+
+
+
+
+
+}
+
